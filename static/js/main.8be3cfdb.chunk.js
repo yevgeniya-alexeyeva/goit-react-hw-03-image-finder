d7d2db27e669fc2700d3ba2e2d2faf1a7cff1f85
Yevgeniya Alexeyeva <y.alekseyeva@gmail.com>
2021-04-28 13:17:39 +0600

    Updates

---
+++ static/js/main.8be3cfdb.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpgoit-react-hw-03-image-finder"]=this["webpackJsonpgoit-react-hw-03-image-finder"]||[]).push([[0],{11:function(e,t,n){"use strict";n.r(t);var c=n(1),r=n.n(c),i=n(3),a=n.n(i),s=(n(8),n(9),n(0));var o=function(){return Object(s.jsx)("div",{className:"App",children:Object(s.jsx)("header",{className:"App-header"})})};a.a.render(Object(s.jsx)(r.a.StrictMode,{children:Object(s.jsx)(o,{})}),document.getElementById("root"))},8:function(e,t,n){},9:function(e,t,n){}},[[11,1,2]]]);
+//# sourceMappingURL=main.8be3cfdb.chunk.js.map
